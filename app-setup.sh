85540ad42cf7993f343bba587231767f4649630c
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-01-28 00:27:53 -0800

    Fail when servers return an error response (eg 404)

--- app-setup.sh
+++ app-setup.sh
@@ -21,27 +21,27 @@ echo 'export PATH="$PATH:${PWD}/bin"' > app/.profile
 chmod +x app/.profile
 
 # Add the cloud-service-broker binary
-(cd app && curl -L -o cloud-service-broker https://github.com/cloudfoundry-incubator/cloud-service-broker/releases/download/0.2.2/cloud-service-broker.linux) && \
+(cd app && curl -f -L -o cloud-service-broker https://github.com/cloudfoundry-incubator/cloud-service-broker/releases/download/0.2.2/cloud-service-broker.linux) && \
     chmod +x app/cloud-service-broker
 
 # Add the brokerpak(s)
-(cd app && curl -LO https://github.com/GSA/eks-brokerpak/releases/download/v0.6.0/eks-services-pack-0.6.0.brokerpak)
-(cd app && curl -LO https://github.com/GSA/datagov-brokerpak/releases/download/v0.6.0/datagov-services-pak-1.0.0.brokerpak)
-(cd app && curl -LO https://github.com/cloudfoundry-incubator/csb-brokerpak-aws/releases/download/1.1.0-rc.5/aws-services-1.1.0-rc.5.brokerpak)
+(cd app && curl -f -LO https://github.com/GSA/eks-brokerpak/releases/download/v0.6.0/eks-services-pack-0.6.0.brokerpak)
+(cd app && curl -f -LO https://github.com/GSA/datagov-brokerpak/releases/download/v0.6.0/datagov-services-pak-1.0.0.brokerpak)
+(cd app && curl -f -LO https://github.com/cloudfoundry-incubator/csb-brokerpak-aws/releases/download/1.1.0-rc.5/aws-services-1.1.0-rc.5.brokerpak)
 
 # Install the Helm binary
-curl -L ${BASE_URL}/${TAR_FILE} |tar xvz && \
+curl -f -L ${BASE_URL}/${TAR_FILE} |tar xvz && \
     mv linux-amd64/helm app/bin/helm && \
     chmod +x app/bin/helm && \
     rm -rf linux-amd64
 
 # Install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
+curl -f -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
     mv kubectl app/bin/kubectl && \
     chmod +x app/bin/kubectl
 
 # Install aws-iam-authenticator
-curl -LO ${AWS_IAM_AUTH_VERSION_URL} && \
+curl -f -LO ${AWS_IAM_AUTH_VERSION_URL} && \
     mv aws-iam-authenticator app/bin/aws-iam-authenticator && \
     chmod +x app/bin/aws-iam-authenticator
     
