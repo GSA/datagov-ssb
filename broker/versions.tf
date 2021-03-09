dee75e1624738eb4fe1ae7c550b767ac361cab61
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-03-08 22:43:28 -0800

    Ran `terraform 0.13upgrade`

--- broker/versions.tf
+++ broker/versions.tf
@@ -1,20 +1,14 @@
 terraform {
   required_providers {
     archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.0.0"
+      source = "hashicorp/archive"
     }
     cloudfoundry = {
       source  = "registry.terraform.io/cloudfoundry-community/cloudfoundry"
       version = "~> 0.13.0"
     }
     random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0.0"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.67"
+      source = "hashicorp/random"
     }
   }
   required_version = ">= 0.13"
