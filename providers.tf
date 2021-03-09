0c56b3f0143111c16602a29687e5faa0d0bcbc5f
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-03-08 22:37:33 -0800

    Refactor providers and backend config into their own files

---
+++ providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+provider "cloudfoundry" {
+  # Configure the CloudFoundry Provider
+  api_url  = var.cf_api_url
+  user     = var.cf_username
+  password = var.cf_password
+}
+
