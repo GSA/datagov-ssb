e73c1365ecd05c0523293938d82da399b2be3fef
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-04-13 14:51:33 -0700

    Set quotas to minimum levels

--- main.tf
+++ main.tf
@@ -8,6 +8,22 @@ data "cloudfoundry_space" "broker_space" {
   org_name = var.broker_space.org
 }
 
+resource "aws_servicequotas_service_quota" "minimum_quotas" {
+  for_each = {
+    "vpc/L-45FE3B85" = 20 # egress-only internet gateways per region
+    "vpc/L-A4707A72" = 20 # internet gateways per region
+    "vpc/L-FE5A380F" = 20 # NAT gateways per AZ
+    "vpc/L-2AFB9258" = 16 # security groups per network interface (16 is the max)
+    "vpc/L-F678F1CE" = 20 # VPCs per region
+    "eks/L-33415657" = 20 # Fargate profiles per cluster
+    "eks/L-23414FF3" = 10 # label pairs per Fargate profile selector
+    "ec2/L-0263D0A3" = 20 # EC2-VPC Elastic IPs
+  }
+  service_code = element(split("/", each.key), 0)
+  quota_code   = element(split("/", each.key), 1)
+  value        = each.value
+}
+
 module "broker_aws" {
   source = "./broker"
 
