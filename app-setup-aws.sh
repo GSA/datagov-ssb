13bb26b1eadb51f36393e67e54a4cfd7f51b1417
Fuhu Xia <fxia@reisystems.com>
2021-05-18 15:01:30 -0400

    update with new BROKERPAK version adding ses

--- app-setup-aws.sh
+++ app-setup-aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-AWS_BROKERPAK_VERSION="1.1.0-rc.5"
+AWS_BROKERPAK_VERSION="1.1.0-rc.6-gsa"
 
 # Set up an app dir and bin dir
 mkdir -p app-aws/bin
@@ -19,6 +19,4 @@ chmod +x app-aws/.profile
     chmod +x app-aws/cloud-service-broker
 
 # Add the brokerpak(s)
-# Temporarily using a hard-coded reference to a version of the AWS brokerpak that grants the rds_superuser role on bind
-# (cd app && curl -f -LO https://github.com/cloudfoundry-incubator/csb-brokerpak-aws/releases/download/${AWS_BROKERPAK_VERSION}/aws-services-${AWS_BROKERPAK_VERSION}.brokerpak)
-(cd app-aws && curl -f -LO https://github.com/GSA/csb-brokerpak-aws/releases/download/1.1.0-rc.5-gsa/aws-services-1.1.0-rc.5-gsa.brokerpak)
+(cd app-aws && curl -f -LO https://github.com/GSA/csb-brokerpak-aws/releases/download/${AWS_BROKERPAK_VERSION}/aws-services-${AWS_BROKERPAK_VERSION}.brokerpak)
