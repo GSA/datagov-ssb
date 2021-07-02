6d0030b2d8a945ba9bb7fc15dbb425cf477eed40
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-07-02 14:03:20 -0700

    Add the SMTP broker deployment

---
+++ app-setup-smtp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+SMTP_BROKERPAK_VERSION="1.0.0"
+
+# Set up an app dir and bin dir
+mkdir -p app-smtp/bin
+
+# Generate a .profile to be run at startup for mapping VCAP_SERVICES to needed
+# environment variables
+cat > app-smtp/.profile << 'EOF'
+# Locate additional binaries needed by the deployed brokerpaks
+export PATH="$PATH:${PWD}/bin"
+EOF
+chmod +x app-smtp/.profile
+
+# Add the cloud-service-broker binary
+(cd app-smtp && curl -f -L -o cloud-service-broker https://github.com/cloudfoundry-incubator/cloud-service-broker/releases/download/0.2.2/cloud-service-broker.linux) && \
+    chmod +x app-smtp/cloud-service-broker
+
+# Add the brokerpak(s)
+(cd app-smtp && curl -f -LO https://github.com/GSA/datagov-brokerpak-smtp/releases/download/${SMTP_BROKERPAK_VERSION}/smtp-services-pak-${SMTP_BROKERPAK_VERSION}.brokerpak)
