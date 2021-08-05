db524862089a5df7520876dc960306f1b9e78067
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-08-05 07:27:18 -0700

    Split the Terraform code for the two boundaries up

--- application-boundary.tf
+++ application-boundary.tf
@@ -1,27 +1,6 @@
-resource "aws_route53_zone" "zone" {
-  count = var.manage_zone ? 1 : 0
-  name  = var.broker_zone
-}
-
 data "cloudfoundry_space" "broker_space" {
   name     = var.broker_space.space
   org_name = var.broker_space.org
-}
-
-resource "aws_servicequotas_service_quota" "minimum_quotas" {
-  for_each = {
-    "vpc/L-45FE3B85" = 20 # egress-only internet gateways per region
-    "vpc/L-A4707A72" = 20 # internet gateways per region
-    "vpc/L-FE5A380F" = 20 # NAT gateways per AZ
-    "vpc/L-2AFB9258" = 16 # security groups per network interface (16 is the max)
-    "vpc/L-F678F1CE" = 20 # VPCs per region
-    "eks/L-33415657" = 20 # Fargate profiles per cluster
-    "eks/L-23414FF3" = 10 # label pairs per Fargate profile selector
-    "ec2/L-0263D0A3" = 20 # EC2-VPC Elastic IPs
-  }
-  service_code = element(split("/", each.key), 0)
-  quota_code   = element(split("/", each.key), 1)
-  value        = each.value
 }
 
 module "broker_eks" {
@@ -39,17 +18,6 @@ module "broker_eks" {
   depends_on = [
     aws_route53_zone.zone
   ]
-}
-
-module "broker_solr" {
-  source = "./broker"
-
-  name          = "ssb-solr"
-  path          = "./app-solr"
-  broker_space  = var.broker_space
-  client_spaces = var.client_spaces
-  enable_ssh    = var.enable_ssh
-  services      = [cloudfoundry_service_instance.k8s_cluster.id]
 }
 
 module "broker_smtp" {
@@ -80,4 +48,15 @@ resource "cloudfoundry_service_instance" "k8s_cluster" {
     update = "90m" # in case of an EKS destroy/create
     delete = "30m"
   }
+}
+
+module "broker_solr" {
+  source = "./broker"
+
+  name          = "ssb-solr"
+  path          = "./app-solr"
+  broker_space  = var.broker_space
+  client_spaces = var.client_spaces
+  enable_ssh    = var.enable_ssh
+  services      = [cloudfoundry_service_instance.k8s_cluster.id]
 }
