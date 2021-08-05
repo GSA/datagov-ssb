138217f8ae9e5656ee97ce8ee15530b5dad9be24
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-08-05 09:26:13 -0700

    Add explicit dependency between k8s instance and broker

--- application-boundary.tf
+++ application-boundary.tf
@@ -48,6 +48,10 @@ resource "cloudfoundry_service_instance" "k8s_cluster" {
     update = "90m" # in case of an EKS destroy/create
     delete = "30m"
   }
+  depends_on = [
+    module.broker_eks
+  ]
+
 }
 
 module "broker_solr" {
