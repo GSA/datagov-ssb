e7f9f038285ae63289f136f366f3898cb65e07d2
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-02-18 17:16:22 -0500

    new: provision standalone eks cluster using terraform only
    Create custom 'brokerpak-eks-terraform-provision' module using eks-brokerpak as source; specify custom parameters to provision with

--- application-boundary.tf
+++ application-boundary.tf
@@ -48,20 +48,32 @@ resource "cloudfoundry_service_instance" "k8s_cluster" {
   ]
 }
 
-resource "cloudfoundry_service_instance" "solrcloud_broker_k8s_cluster" {
-  name         = "ssb-solrcloud-k8s"
-  space        = data.cloudfoundry_space.broker_space.id
-  service_plan = module.broker_eks.plans["aws-eks-service/raw"]
-  tags         = ["k8s"]
-  json_params  = "{\"mng_min_capacity\": 8, \"mng_max_capacity\": 12, \"mng_desired_capacity\": 10}"
-  timeouts {
-    create = "60m"
-    update = "90m" # in case of an EKS destroy/create
-    delete = "40m"
-  }
-  depends_on = [
-    module.broker_eks
-  ]
+# resource "cloudfoundry_service_instance" "solrcloud_broker_k8s_cluster" {
+#   name         = "ssb-solrcloud-k8s"
+#   space        = data.cloudfoundry_space.broker_space.id
+#   service_plan = module.broker_eks.plans["aws-eks-service/raw"]
+#   tags         = ["k8s"]
+#   json_params  = "{\"mng_min_capacity\": 8, \"mng_max_capacity\": 12, \"mng_desired_capacity\": 10}"
+#   timeouts {
+#     create = "60m"
+#     update = "90m" # in case of an EKS destroy/create
+#     delete = "40m"
+#   }
+#   depends_on = [
+#     module.broker_eks
+#   ]
+# }
+
+module "brokerpak-eks-terraform-provision" {
+  source = "github.com/GSA/datagov-brokerpak-eks//terraform/provision?ref=main"
+
+  subdomain            = var.eks_terraform_subdomain
+  region               = var.eks_terraform_region
+  zone                 = var.eks_terraform_zone
+  instance_name        = var.eks_terraform_instance_name
+  mng_min_capacity     = var.eks_terraform_mng_min_capacity
+  mng_max_capacity     = var.eks_terraform_mng_max_capacity
+  mng_desired_capacity = var.eks_terraform_mng_desired_capacity
 }
 
 module "broker_solrcloud" {
--- terraform.development.tfvars
+++ terraform.development.tfvars
@@ -8,3 +8,12 @@ broker_space = {
 }
 broker_zone = "ssb-dev.data.gov"
 manage_zone = true
+
+# Terraform parameters for eks
+eks_terraform_subdomain            = "solrcloud-k8s"
+eks_terraform_region               = "us-west-2"
+eks_terraform_zone                 = "ssb-dev.data.gov"
+eks_terraform_instance_name        = "solrcloud"
+eks_terraform_mng_min_capacity     = 1
+eks_terraform_mng_max_capacity     = 11
+eks_terraform_mng_desired_capacity = 10
