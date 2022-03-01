cd11648866a7014fd8b2833bf082e569b4666d4b
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-03-01 17:56:55 -0500

    new: install tools as TERRAFORM_PRE_RUN
    Since dflook action is a separate container, the tools need to be pulled in properly.  The terraform working directory becomes part of the path for that step, so installing the tools there works :)

--- install-tools.sh
+++ install-tools.sh
@@ -10,23 +10,16 @@ AWS_IAM_AUTH_VERSION_URL="https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2
 BASE_URL="https://get.helm.sh"
 TAR_FILE="helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 
-# Set up an app dir and bin dir
-mkdir -p bin
-
 # Install the Helm binary
 curl -f -L ${BASE_URL}/${TAR_FILE} |tar xvz && \
-    mv linux-amd64/helm bin/helm && \
-    chmod +x bin/helm && \
+    mv linux-amd64/helm helm && \
+    chmod +x helm && \
     rm -rf linux-amd64
 
 # Install kubectl
 curl -f -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
-    mv kubectl bin/kubectl && \
-    chmod +x bin/kubectl
+    chmod +x kubectl
 
 # Install aws-iam-authenticator
 curl -f -LO ${AWS_IAM_AUTH_VERSION_URL} && \
-    mv aws-iam-authenticator bin/aws-iam-authenticator && \
-    chmod +x bin/aws-iam-authenticator
-    
-
+    chmod +x aws-iam-authenticator
