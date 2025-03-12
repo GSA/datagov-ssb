e224243a2e9438a3ae2688ea5471c204ef6a69ea
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2025-03-12 15:18:19 -0500

    Update provider versions

--- broker/versions.tf
+++ broker/versions.tf
@@ -2,13 +2,15 @@ terraform {
   required_providers {
     archive = {
       source = "hashicorp/archive"
+      version = "~> 2.7.0"
     }
     cloudfoundry = {
       source  = "registry.terraform.io/cloudfoundry-community/cloudfoundry"
-      version = "~> 0.14.0"
+      version = "~> 0.54.0"
     }
     random = {
       source = "hashicorp/random"
+      version = "~> 3.7.1"
     }
   }
   required_version = ">= 0.13"
