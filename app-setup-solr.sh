9164d2bfbb2a1a116dfc4a00ba7c9bbddce073bb
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2025-05-09 13:52:38 -0500

    Use updated manual release of Solr brokerpak

--- app-setup-solr.sh
+++ app-setup-solr.sh
@@ -2,7 +2,7 @@
 set -ex
 APP_NAME=app-solr
 CSB_VERSION="v2.5.6"
-DATAGOV_BROKERPAK_SOLR_VERSION="v2.2.0"
+DATAGOV_BROKERPAK_SOLR_VERSION="v2.4.0rc"
 
 # Install zip for AWS Lambda restarts of solr
 # Install pip to install slack_sdk for Slack notifications
